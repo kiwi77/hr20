e645767d333f830a24801875b2ca54d56fc0166c
Michael Schultz <github@keyb.de>
2010-05-17 03:57:17 +0200

    the adaption run is possible

--- compile_all.sh
+++ compile_all.sh
@@ -2,9 +2,12 @@
 #
 #
 
-
+# Define here you ip addresses:
 HR20ipAddress="10.40.1.31"
 ZBusGatewayIP=10.0.0.94
+
+#
+#
 
 if test ! -d ./backup ; then mkdir ./backup ; fi
 
@@ -17,15 +20,18 @@ cd ../zbusloader/					&&
 make clean						&&
 make							&&
 cd -							&&
+
+Error=$? ; if [ $Error != 0 ] ; then exit 1 ; fi
+
 if test ! -z "${HR20ipAddress}" ; then
-	echo "bootloader" | socat stdio tcp4:10.40.1.31:2701
+	echo "bootloader" | socat stdio tcp4:${HR20ipAddress}:2701,connect-timeout=4
 fi
 if test ! -z "${ZBusGatewayIP}" ; then
 	while true ; do
 		../zbusloader/contrib/zbusloader -p 128 -t 5 -H ${ZBusGatewayIP} -f ../ethersex/ethersex.bin ; Error=$?
 		if [ ${Error} = 0 ] ; then break ; fi
 		if test ! -z "${HR20ipAddress}" ; then
-			echo "bootloader" | socat stdio tcp4:10.40.1.31:2701
+			echo "bootloader" | socat stdio tcp4:${HR20ipAddress}:2701,connect-timeout=4
 		else
 			exit 1
 		fi
@@ -33,7 +39,7 @@ if test ! -z "${ZBusGatewayIP}" ; then
 	if test ! -z "${HR20ipAddress}" ; then
 		echo "Waiting 10 seconds to boot the HR20"
 		sleep 10
-		date +"time "%s | socat stdio tcp4:${HR20ipAddress}:2701
+		date +"time "%s | socat stdio tcp4:${HR20ipAddress}:2701,connect-timeout=4
 	fi
 fi
 # view free space on chip, and clean it things...
